8abc52726c17b079db8d4c0ee80e5522db8a1812
David Huerta <dhuerta23@gmail.com>
2016-09-24 19:34:53 +0200

    First Commit

---
+++ state-machine-kata/src/AppBundle/Command/HiCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class HiCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('hi')
+            ->setDescription('Hello world command.')
+            ->addArgument('name', InputArgument::OPTIONAL, 'Hello to NAME')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        if (strtolower($input->getArgument('name')) == "carlos  ") {
+            $output->writeln('Hallo Knallkopf.');
+        } elseif ($input->getArgument('name')) {
+            $output->writeln('Que paso?? '.$input->getArgument('name').' el sabiondo!!!');
+        } else {
+            $output->writeln('Hallo.');
+        }
+    }
+
+}
